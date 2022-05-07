71f4c5a561cf1104b28657efb946b7c8310ec769
syfxlin <i@ixk.me>
2022-05-07 22:38:44 +0800

    :sparkles: feature: New design depker v2

--- .depker/depker.ts
+++ .depker/depker.ts
@@ -1,22 +1,17 @@
 /// <reference path="https://raw.githubusercontent.com/syfxlin/depker/master/src/types/index.ts" />
 
-export const deploy = async () => {
-  const dockerfile = depker.template.nodejs_static({
-    nginx: { root: "out" },
-  });
-  const image = await depker.docker.build("syfxlin/home", {
-    dockerfile_contents: dockerfile,
-  });
-  const compose = depker.compose.deployment("home", {
-    services: {
-      web: {
-        image,
-        traefik: {
-          domain: ["ixk.me", "www.ixk.me"],
-          tls: true,
-        },
-      },
+export const deploy = depker.docker.of(() => ({
+  name: "home",
+  image: "syfxlin/home",
+  build: {
+    dockerfile_contents: depker.template.nodejs_static({
+      nginx: { root: "out" },
+    }),
+  },
+  run: {
+    traefik: {
+      domain: ["ixk.me", "www.ixk.me"],
+      tls: true,
     },
-  });
-  await compose.up();
-};
+  },
+}));
