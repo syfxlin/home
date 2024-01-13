1aa7385d6ec0538072d4b18da5be38cba59f00af
Otstar Lin <i@ixk.me>
2024-01-13 20:31:00 +0800

    Update public.d.ts

--- src/env/public.d.ts
+++ src/env/public.d.ts
@@ -1,5 +1,5 @@
 // public
 export const IS_DEV: boolean;
 export const COLINE_LANGUAGE: string;
-export const COLINE_LANGUAGE = string | undefined;
+export const COLINE_GITHUB_REPO: string | undefined;
 export const COLINE_GOOGLE_ANALYTICS: string | undefined;
