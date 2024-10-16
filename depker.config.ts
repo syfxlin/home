6b42e142a69a40365acd452a43b303eef7a3b6ec
syfxlin <i@ixk.me>
2024-10-17 00:37:14 +0800

    fix(depker): Update depker config

--- depker.config.ts
+++ depker.config.ts
@@ -1,26 +1,22 @@
 import { depker, docker, nextjs } from "https://raw.githubusercontent.com/syfxlin/depker/master/mod.ts";
 
-const app = depker();
+depker.master(docker({ type: "https" }));
+depker.runner(docker({ type: "local" }));
 
-app.master(docker({ type: "https" }));
-app.runner(docker({ type: "local" }));
-
-app.service(
+depker.use(
   nextjs({
     name: "home",
     domain: ["ixk.me", "www.ixk.me"],
     tls: true,
     secrets: {
-      NEXT_PUBLIC_COLINE_LANGUAGE: "@HOME_COLINE_LANGUAGE",
-      NEXT_PUBLIC_COLINE_GOOGLE_ANALYTICS: "@HOME_COLINE_GOOGLE_ANALYTICS",
-      NEXT_PUBLIC_COLINE_GITHUB_REPO: "@HOME_COLINE_GITHUB_REPO",
-      COLINE_GITHUB_TOKEN: "@HOME_COLINE_GITHUB_TOKEN",
-      KEYSTATIC_SECRET: "@HOME_KEYSTATIC_SECRET",
-      KEYSTATIC_GITHUB_CLIENT_ID: "@HOME_KEYSTATIC_GITHUB_CLIENT_ID",
-      KEYSTATIC_GITHUB_CLIENT_SECRET: "@HOME_KEYSTATIC_GITHUB_CLIENT_SECRET",
-      NEXT_PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: "@HOME_KEYSTATIC_GITHUB_APP_SLUG",
+      NEXT_PUBLIC_COLINE_LANGUAGE: "@NEXT_PUBLIC_COLINE_LANGUAGE",
+      NEXT_PUBLIC_COLINE_GOOGLE_ANALYTICS: "@NEXT_PUBLIC_COLINE_GOOGLE_ANALYTICS",
+      NEXT_PUBLIC_COLINE_GITHUB_REPO: "@NEXT_PUBLIC_COLINE_GITHUB_REPO",
+      COLINE_GITHUB_TOKEN: "@COLINE_GITHUB_TOKEN",
+      KEYSTATIC_SECRET: "@KEYSTATIC_SECRET",
+      KEYSTATIC_GITHUB_CLIENT_ID: "@KEYSTATIC_GITHUB_CLIENT_ID",
+      KEYSTATIC_GITHUB_CLIENT_SECRET: "@KEYSTATIC_GITHUB_CLIENT_SECRET",
+      NEXT_PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: "@NEXT_PUBLIC_KEYSTATIC_GITHUB_APP_SLUG",
     },
   }),
 );
-
-export default app;
